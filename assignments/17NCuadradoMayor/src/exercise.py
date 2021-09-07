c062968f67419bdefc7973027e5b126aa7fd3c82
JorgeRuedaG <89436064+JorgeRuedaG@users.noreply.github.com>
2021-09-07 17:28:16 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -1,7 +1,11 @@
 
 
 def main():
-    #Escribe tu código debajo de esta línea
+    num=int(input("Escribe un número:"))
+    var=1
+    while var**2<=num:
+        var=var+1
+    print(var)
     pass
 
 if __name__=='__main__':
