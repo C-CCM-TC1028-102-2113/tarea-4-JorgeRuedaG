0c3d4b0fb0181b907ba82554f72b956f21ff1128
JorgeRuedaG <89436064+JorgeRuedaG@users.noreply.github.com>
2021-09-07 17:22:23 -0500

    Update exercise.py

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,5 +1,11 @@
 def main():
-    #escribe tu código abajo de esta línea
+    n=int(input("Escribe un numero entero:"))
+    for i in range(1, n+1):
+        if i%2:
+            print(i,"-#")
+        if not i%2:
+            print(i,"-%")
+    
     pass
 
 if __name__=='__main__':   
